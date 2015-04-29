1dbfe0209d0de1596904a522f894efe912199ee8
715lvzhihao <1597791782a@2980.com>
2015-04-29 21:43:52 +0800

    test

---
+++ AppletInOut.java
@@ -0,0 +1,24 @@
+package test2;
+
+import java.applet.*; 
+import java.awt.*;  
+import java.awt.event.*;  
+public class AppletInOut extends Applet implements ActionListener 
+{ 
+Label prompt; 
+TextField input,output; 
+public void init() 
+{ 
+ prompt=new Label("«Î ‰»Î:"); 
+input=new TextField(6); 
+output=new TextField(20); 
+add(prompt); 
+add(input); 
+add(output); 
+input.addActionListener(this); 
+} 
+public void actionPerformed(ActionEvent e) 
+ { 
+ output.setText(input.getText()+",welcome!"); 
+} 
+}  
