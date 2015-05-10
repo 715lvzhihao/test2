0b01be27836663f911a4a972f996184e10bce8e4
715lvzhihao <1597791782a@2980.com>
2015-05-10 22:59:41 +0800

    Test

---
+++ RingTest.java
@@ -0,0 +1,54 @@
+package test2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RingTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRing() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testGetInnerRadius() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testGetOuterRadius() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testSetInnerRadius() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testSetouterRadius() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testSetColor() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testGetArea() {
+//		fail("尚未实现");
+	}
+
+	@Test
+	public void testMain() {
+//		fail("尚未实现");
+	}
+
+}
